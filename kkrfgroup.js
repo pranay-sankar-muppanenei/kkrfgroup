6ba6efba139fbe71d1d4f2d31fbdb9082a73fda1
pranay-sankar-muppanenei <122502110+pranay-sankar-muppanenei@users.noreply.github.com>
2024-10-18 11:02:06 +0530

    Add files via upload

---
+++ kkrfgroup.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById('menu')
+
+/*
+function clickmenu(){
+    let we=document.getElementById('weare')
+    let nav=document.getElementById('navblack')
+    we.style.color='white'
+    nav.style.backgroundColor='black'
+
+}
+*/
